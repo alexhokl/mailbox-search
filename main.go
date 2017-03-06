35c036af779d44d1b0ae8a754ffbc828ad720066
alexhokl <alexhokl@gmail.com>
2017-03-06 09:28:53 +0800

    Removed hard-coded parameters

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/mail"
 	"os"
 	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
-	targets := []string{}
-	isSentMode := true
-	domain := ""
-	startDate := time.Date(2016, time.January, 1, 0, 0, 0, 0, time.UTC)
-	endDate := time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC)
+	targets, errTargets := getTargets()
+	if errTargets != nil {
+		fmt.Println(errTargets)
+		return
+	}
+	isSentMode, errMode := isSentMode()
+	if errMode != nil {
+		fmt.Println(errMode)
+		return
+	}
+	domain, errDomain := getDomain()
+	if errDomain != nil {
+		fmt.Println(errDomain)
+		return
+	}
+	startDate, errStartDate := getStartDate()
+	if errStartDate != nil {
+		fmt.Println(errStartDate)
+		return
+	}
+	endDate, errEndDate := getEndDate()
+	if errEndDate != nil {
+		fmt.Println(errEndDate)
+		return
+	}
 	directories := os.Args[1:]
 	for _, d := range directories {
 		files, errDir := ioutil.ReadDir(d)
@@ -121,4 +143,57 @@ func hasTargets(list []*mail.Address, targets []string) bool {
 func isInDateRange(mailMessage *mail.Message, startDate time.Time, endDate time.Time) bool {
 	date, _ := mailMessage.Header.Date()
 	return date.After(startDate) && date.Before(endDate)
+}
+
+func isSentMode() (bool, error) {
+	valStr, err := getEnvironmentVariable("MAILBOX_SEARCH_IS_SENT")
+	if err != nil {
+		return false, err
+	}
+	return strconv.ParseBool(valStr)
+}
+
+func getTargets() ([]string, error) {
+	valStr, err := getEnvironmentVariable("MAILBOX_SEARCH_TARGETS")
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(valStr, ","), nil
+}
+
+func getDomain() (string, error) {
+	return getEnvironmentVariable("MAILBOX_SEARCH_DOMAIN")
+}
+
+func getStartDate() (time.Time, error) {
+	valStr, err := getEnvironmentVariable("MAILBOX_SEARCH_START_DATE")
+	if err != nil {
+		return time.Now(), err
+	}
+	return getDate(valStr)
+}
+
+func getEndDate() (time.Time, error) {
+	valStr, err := getEnvironmentVariable("MAILBOX_SEARCH_END_DATE")
+	if err != nil {
+		return time.Now(), err
+	}
+	return getDate(valStr)
+}
+
+func getEnvironmentVariable(name string) (string, error) {
+	val := os.Getenv(name)
+	if val == "" {
+		return "", errors.New(fmt.Sprintf("Environment variable %s is not set"))
+	}
+	return val, nil
+}
+
+func getDate(valStr string) (time.Time, error) {
+	const format = "2016-12-31T12:00:00Z"
+	val, err := time.ParseInLocation(format, valStr, nil)
+	if err != nil {
+		return time.Now(), errors.New(fmt.Sprintf("Unable to parse date. Expected format %s", format))
+	}
+	return val, nil
 }
