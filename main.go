e04aebcc82184fc51646fd0e467bf467f3eef386
alexhokl <alexhokl@gmail.com>
2017-03-07 20:39:36 +0800

    Fixed a bug in error message

--- main.go
+++ main.go
@@ -229,7 +229,7 @@ func getEndDate() (time.Time, error) {
 func getEnvironmentVariable(name string) (string, error) {
 	val := os.Getenv(name)
 	if val == "" {
-		return "", errors.New(fmt.Sprintf("Environment variable %s is not set"))
+		return "", errors.New(fmt.Sprintf("Environment variable %s is not set", name))
 	}
 	return val, nil
 }
